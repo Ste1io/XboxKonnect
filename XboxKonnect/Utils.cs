0b8076f871e2d85967a28a58e6cef0fdaf670ed9
Stelio Kontos <37424493+StelioKontosXBL@users.noreply.github.com>
2022-06-20 11:38:49 -0500

    Update subnet discovery util functions

--- XboxKonnect/Utils.cs
+++ XboxKonnect/Utils.cs
@@ -1,26 +1,30 @@
 ﻿/*
  * XboxKonnect - Xbox Auto Discovery API
- * 
+ *
  * Created: 10/24/2017
  * Author:  Daniel McClintock (alias: Stelio Kontos)
- * 
+ *
  * Copyright (c) 2017 Daniel McClintock
- * 
+ *
  */
 
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
 
 namespace SK.XboxKonnect
 {
 	internal static class Utils
 	{
-		internal static IPEndPoint? GetHostEndPoint()
+		internal static IEnumerable<(NetworkInterface NIC, IPAddress BroadcastAddress)> GetSubnets()
 		{
-			using Socket udpSocket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-			udpSocket.Connect("10.0.20.20", 31337);
-			return udpSocket.LocalEndPoint as IPEndPoint;
+			return NetworkInterface.GetAllNetworkInterfaces()
+				.Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+				.SelectMany(adapter => adapter.GetIPProperties().UnicastAddresses
+					.Where(a => a.Address.AddressFamily == AddressFamily.InterNetwork)
+					.Select(ipInfo => (adapter, ipInfo.Address.GetBroadcastAddress()))
+				);
 		}
 
 		internal static IPAddress GetBroadcastAddress(this IPAddress ip)
